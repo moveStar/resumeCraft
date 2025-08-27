821f583bd2736aa16529b15e4eb14e76191ba077
张维娟 <15002518569@163.com>
2025-08-27 15:29:06 +0800

    intinal

---
+++ src/components/CallToAction.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Description: 
+ * @Author: zhangweijuan
+ * @Date: 2025-08-27 10:53:10
+ * @LastEditTime: 2025-08-27 10:54:44
+-->
+<template>
+  <div class="call-to-action">
+    <h2>立即创建您的专业简历</h2>
+    <p>免费试用，无需信用卡。3 分钟内即可获得一份优化后的专业简历</p>
+    <button>开始免费试用</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CallToAction',
+});
+</script>
+
+<style scoped>
+/* 添加样式 */
+</style>
